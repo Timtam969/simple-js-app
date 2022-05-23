e312d2b21386e0afc4083b35cbd47ab2e9a09bb0
Timothy Losper <tim_losper@yahoo.com>
2022-05-23 15:07:48 +0200

    changing quotation marks to single

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,6 @@
-let pokemonList= [{name: "Charizard", height: "1.7m", type:["Monster","Dragon"], abilities:["Blaze","Solor-Power"]},
-                {name: "Fearow", height: "1.2m", type:["Flying"], abilities:["Keen-eye","Sniper"]},
-                {name: "Nidoking", height: "1.4m", type:["Monster","Field"], abilities:["Poison-point","Rivalry","Sheer-force"]},
-                {name: "Tentacruel", height: "1.6m", type:["Water3"], abilities:["Clear-body","Rain-dish","Liquid-ooze"]},
-                {name: "Wailord", height: "14.5m", type:["Field","Water2"], abilities:["Oblivious","Water-veil","Pressure"]},
-              ]
+let pokemonList= [{name: 'Charizard', height: 1.7, type:['Monster","Dragon'], abilities:['Blaze','Solor-Power']},
+                {name: 'Fearow', height: 1.2, type:['Flying'], abilities:['Keen-eye','Sniper']},
+                {name: 'Nidoking', height: 1.4, type:['Monster','Field'], abilities:['Poison-point','Rivalry','Sheer-force']},
+                {name: 'Tentacruel', height: 1.6, type:['Water3'], abilities:['Clear-body','Rain-dish','Liquid-ooze']},
+                {name: 'Wailord', height: 14.5, type:['Field','Water2'], abilities:['Oblivious','Water-veil','Pressure']},
+              ];
